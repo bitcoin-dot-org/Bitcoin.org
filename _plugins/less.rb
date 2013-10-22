15790cb918a11fa390069f67d8e42aefb8f269ae
Saivann <saivann@gmail.com>
2013-10-22 04:55:54 -0400

    Prevent less.rb to process the same files repeatedly

--- _plugins/less.rb
+++ _plugins/less.rb
@@ -16,51 +16,50 @@ module Jekyll
 
     def render(context)
       site = context.registers[:site]
-
+      config = context.registers[:site].config
+      if !config.has_key?('lessdone')
+        config['lessdone'] = {}
+      end
       less_dir = File.join(site.source, '_less')
-
       if File.symlink?(less_dir)
         return "LessCSS directory '#{less_dir}' cannot be a symlink"
       end
-
-      if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
-        return "LessCSS file '#{@file}' contains invalid characters or sequences"
-      end
-
-      Dir.chdir(less_dir) do
-        choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-        if choices.include?(@file)
-
-          if /\.css$/.match(@file)
-            f = file = File.new(@file, "r")
+      #Process file only if it hasn't been processed yet
+      if !config['lessdone'].has_key?(@file)
+        Dir.chdir(less_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(@file)
+            #Generate file content with less, those that end with .css
+            if /\.css$/.match(@file)
+              f = file = File.new(@file, "r")
+            else
+              source = File.read(@file)
+              f = IO.popen("lessc -", "w+")
+              f.write(source)
+              f.close_write()
+            end
+            #Keep MD5 hash of the file as the file name
+            css = f.readlines().join()
+            digest = Digest::MD5.hexdigest(css)
+            css_file = digest + ".css"
+            css_path = File.join(site.dest, css_file)
+            #Write final file
+            if !File.directory?(site.dest)
+              Dir.mkdir(site.dest)
+            end
+            File.open(css_path, "w") do |f|
+              f.write(css)
+            end
+            site.static_files << LessCSSFile.new(site, site.source, '', css_file)
+            #Save the filename so the file isn't processed again
+            config['lessdone'][@file] = css_file
           else
-            source = File.read(@file)
-            f = IO.popen("lessc -", "w+")
-            f.write(source)
-            f.close_write()
+            return "LessCSS file '#{@file}' not found in '#{less_dir}'"
           end
-
-          css = f.readlines().join()
-
-          digest = Digest::MD5.hexdigest(css)
-
-          css_file = digest + ".css"
-          css_path = File.join(site.dest, css_file)
-          
-          if !File.directory?(site.dest)
-            Dir.mkdir(site.dest)
-          end
-          File.open(css_path, "w") do |f|
-            f.write(css)
-          end
-
-          site.static_files << LessCSSFile.new(site, site.source, '', css_file)
-        
-          '<link rel="stylesheet" type="text/css" href="/' + css_file + '" />'
-        else
-          return "LessCSS file '#{@file}' not found in '#{less_dir}'"
         end
       end
+      #Print CSS link in HTML layout
+      '<link rel="stylesheet" type="text/css" href="/' + config['lessdone'][@file] + '" />'
     end
   end
 
