4bdca82894cc921af5dc031f62dcc8a1bf2f7e5a
Saivann <saivann@gmail.com>
2014-02-19 21:43:05 -0500

    Escape special HTML characters on the events page

---
+++ _plugins/htmlescape.rb
@@ -0,0 +1,16 @@
+#htmlescape espaces special html characters. This is a replacement for
+#CGI::escapeHTML, which has an inconsistent behavior with single quotes
+#on different ruby versions ( 1.9 and 2.0 ).
+
+#Example:
+# {{ page.title | htmlescape }}
+
+module Entities
+
+  def htmlescape(input)
+    input.gsub(/['&\"<>]/, { "'" => '&apos;', '&' => '&amp;', '"' => '&quot;', '<' => '&lt;', '>' => '&gt;' })
+  end
+
+  Liquid::Template.register_filter self
+
+end
