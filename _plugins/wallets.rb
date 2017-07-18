43ec650bd4e48b1818975cf9d8b1a6dfb7ec098f
Igor Kuzmenko <igor@kuzzmi.com>
2017-07-18 17:49:58 +0200

    Adds support for translation of platform pages

--- _plugins/wallets.rb
+++ _plugins/wallets.rb
@@ -22,12 +22,31 @@ module Jekyll
     end
   end
 
+  class PlatformPage < Page
+    def initialize(site, base, dir, platform, os, lang)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'wallet-platform.html')
+      self.data['platform'] = platform
+      self.data['os'] = os
+      self.data['id'] = ['wallets', platform['name'], os['name']].join('-')
+      self.data['lang'] = lang
+    end
+  end
+
   class WalletsPageGenerator < Generator
     safe true
 
     def generate(site)
       # Get the collection of wallets from _wallets
       walletsCol = site.collections['wallets'];
+
+      # Get the collection of wallets from _wallets
+      platformsCol = site.collections['platforms'];
 
       # Output dir
       # TODO: Make this configurable and "translatable"
@@ -49,37 +68,37 @@ module Jekyll
         locs[lang] = YAML.load_file("_translations/"+file)[lang]
       end
 
-      puts('----- Generating wallet pages -----')
-
       # Getting information about each found wallet
-      walletsCol.docs.each do |doc|
-        file = doc.path
-        wallet = YAML.load_file(file)
-        walletPlatforms = wallet['platform']
+      locs.each do |lang,value|
+        platformsCol.docs.each do |doc|
+          file = doc.path
+          data = YAML.load_file(file)
+          platform = data['platform']
+          os = data['os']
+          dir = File.join(platform['name'], os['name'])
+          site.pages << PlatformPage.new(site, site.source, File.join(lang, walletsDir, dir), platform, os, lang)
+        end
 
-        puts(wallet['id'] + ' is loaded. Generating pages...')
+        walletsCol.docs.each do |doc|
+          file = doc.path
+          wallet = YAML.load_file(file)
+          walletPlatforms = wallet['platform']
 
-        # Going through all available combinations of
-        # platforms and OSes
-        walletPlatforms.each do |platform|
-          platform['os'].each do |os|
+          # Going through all available combinations of
+          # platforms and OSes
+          walletPlatforms.each do |platform|
+            platform['os'].each do |os|
 
-            # This allows generation only of valid wallet pages
-            if platform['name']
-
-              locs.each do |lang,value|
+              # This allows generation only of valid wallet pages
+              if platform['name']
                 dir = File.join(platform['name'], os['name'], wallet['id'])
                 site.pages << WalletPage.new(site, site.source, File.join(lang, walletsDir, dir), wallet, platform, os, lang)
               end
-            end
 
+            end
           end
         end
-
-        puts(wallet['id'] + ' is processed.')
       end
-
-      puts('----- Wallet pages generated -----')
 
     end
   end
