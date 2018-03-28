e529c7846baf1549f898d9b6e60e53eb87a34c13
alexcherman <sashacherman@gmail.com>
2018-03-28 15:35:54 +0200

    blog page desktop

--- js/loadMorePosts.js
+++ js/loadMorePosts.js
@@ -1,12 +1,12 @@
 $(function () {
-  var list = $(".event");
+  var list = $(".post");
   list.slice(0, 6).show();
   $("#loadMore").on('click', function (e) {
     e.preventDefault();
-    $(".event:hidden").slice(0, 6).slideDown();
-    if ($(".event:hidden").length == 0) {
+    $(".post:hidden").slice(0, 6).slideDown();
+    if ($(".post:hidden").length == 0) {
       $("#loadMore").fadeOut('slow');
-      $(".eventtable").addClass("is-open")
+      $(".post-list").addClass("is-open")
     }
   });
 });
