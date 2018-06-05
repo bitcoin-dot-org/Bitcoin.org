62f15ccec74f4a252ab2e3fb79ca91f6ad8cbb50
Will Binns <will@trek.io>
2018-06-04 18:24:16 -0600

    main: Resolve additional JSHint issue

--- js/main.js
+++ js/main.js
@@ -643,7 +643,7 @@ function walletMenuAccordion() {
       this.classList.toggle("is-expanded");
 
       for (var index = 0; index < tabs.length; index++) {
-        if (!(this === tabs[index])) {
+        if (this !== tabs[index]) {
           tabs[index].classList.remove("is-expanded");
         }
       }
