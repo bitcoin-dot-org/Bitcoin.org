4c1cdc8ab8d34852ffa56903b5a6db15fc6b9bfd
Will Binns <will@trek.io>
2019-01-12 06:41:41 -0600

    rubocop: Resolve indentation inconsistencies

--- _build/txpreview/addlang.rb
+++ _build/txpreview/addlang.rb
@@ -14,8 +14,8 @@ end
 contents = File.read(PATH + '/_config.yml')
 
 if Regexp.new('langsorder:.*?' + "\n" + '- \'' + la + '\'' + "\n", Regexp::MULTILINE).match(contents).nil?
-	 contents.gsub!(Regexp.new("(langsorder:.*?)\n\n", Regexp::MULTILINE), '\1' + "\n" + '- \'' + la + '\'' + "\n\n")
-	 contents.gsub!(Regexp.new("(langs:.*?)\n\n", Regexp::MULTILINE), '\1' + "\n" + '  \'' + la + '\': \'' + la + '\'' + "\n\n")
+  contents.gsub!(Regexp.new("(langsorder:.*?)\n\n", Regexp::MULTILINE), '\1' + "\n" + '- \'' + la + '\'' + "\n\n")
+  contents.gsub!(Regexp.new("(langs:.*?)\n\n", Regexp::MULTILINE), '\1' + "\n" + '  \'' + la + '\': \'' + la + '\'' + "\n\n")
 end
 
 File.open(PATH + '/_config.yml', 'w') do |file|
