dd22fd7f0ad67e99274189aa5ca189de6d958931
alexcherman <sashacherman@gmail.com>
2019-06-24 16:53:42 +0200

    Wallet page redesign

--- js/main.js
+++ js/main.js
@@ -733,3 +733,22 @@ function handleDevDocsRedirect(name) {
   }
 
 }
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function updateQueryStringParameter(key, value) {
+  var uri = window.location.href;
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    return uri + separator + key + "=" + value;
+  }
+}
