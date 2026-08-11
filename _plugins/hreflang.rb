d7c145ee61f9901ad37fd9af5b2859eb4071911c
Cøbra <domain@bitcoin.org>
2026-08-11 16:35:57 +0100

    Update hreflang.rb

--- _plugins/hreflang.rb
+++ _plugins/hreflang.rb
@@ -9,6 +9,7 @@ module Jekyll
     def generate(site)
       origin = site.config.fetch('url', 'https://bitcoin.org')
                           .sub(%r{/$}, '')
+
       language_order = Array(site.config['langsorder'])
       groups = Hash.new { |hash, id| hash[id] = [] }
 
@@ -18,9 +19,18 @@ module Jekyll
 
         next if id.nil? || lang.nil?
 
+        # Redirect pages inherit id: index from index.html, but they are
+        # not translated versions of the language homepage.
+        next if page.data['redirect']
+
         path = public_path(page.url)
 
-        # Excludes /, /404.html and other non-localized pages.
+        # Only /<language>/ belongs to the homepage translation group.
+        # This also excludes /404.html and any other page that
+        # unintentionally inherits id: index.
+        next if id == 'index' && path != "/#{lang}/"
+
+        # Exclude non-localized pages such as the root homepage.
         next unless path.start_with?("/#{lang}/")
 
         groups[id] << [page, lang, path]
@@ -29,14 +39,20 @@ module Jekyll
       groups.each do |id, entries|
         # Use only one URL for each language.
         by_language = {}
-        entries.each { |entry| by_language[entry[1]] ||= entry }
+
+        entries.each do |entry|
+          lang = entry[1]
+          by_language[lang] ||= entry
+        end
 
         # A page with only one language has no translated alternatives.
         next if by_language.length < 2
 
-        alternates = by_language.values.sort_by do |_page, lang, _path|
+        alternates = by_language.values
+                                .sort_by do |_page, lang, _path|
           language_order.index(lang) || language_order.length
-        end.map do |_page, lang, path|
+        end
+                                .map do |_page, lang, path|
           {
             'lang' => lang.tr('_', '-'),
             'url'  => origin + path
@@ -46,7 +62,8 @@ module Jekyll
         entries.each do |page, _lang, _path|
           page.data['hreflang_alternates'] = alternates
 
-          # / selects or redirects to the appropriate language.
+          # The root URL selects or redirects to the appropriate
+          # language and is therefore the homepage x-default.
           if id == 'index'
             page.data['hreflang_x_default'] = origin + '/'
           end
